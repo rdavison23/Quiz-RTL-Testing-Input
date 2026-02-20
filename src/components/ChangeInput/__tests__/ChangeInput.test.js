ed1389b9cd64963b85e29e8294f0a4a2404d10ad
Your Name <your-email@example.com>
2026-02-19 16:56:29 -0800

    added basic test for changeINPut

--- src/components/ChangeInput/__tests__/ChangeInput.test.js
+++ src/components/ChangeInput/__tests__/ChangeInput.test.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ChangeInput from '../ChangeInput';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render, fireEvent, cleanup, screen } from '@testing-library/react';
+
+test('checking for anonymous', async () => {
+  render(<ChangeInput />);
+  const welcome = screen.getByText(/Welcome/i);
+  console.log(welcome.innerHTML);
+  expect(welcome.innerHTML).toEqual('Welcome, Anonymous User!');
+});
